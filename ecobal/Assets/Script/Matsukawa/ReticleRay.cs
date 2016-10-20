1419a2608eaee9599045ded32c354f61686717f3
松川修平 <m01144078b@edu.teu.ac.jp>
2016-10-20 18:42:15 +0900

    [松川][追加]Reticle用のScriptの作成。Raycast関連が終わったので一度上げときます。

---
+++ ecobal/Assets/Script/Matsukawa/ReticleRay.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReticleRay : MonoBehaviour
+{
+    public float rayRange;
+    private RaycastHit hit;
+    public LayerMask rayMask;
+    private bool enemyHit;
+
+    public Sprite nomalReticle;
+    public Sprite hitReticle;
+
+    public bool debugMode;
+
+    void Start()
+    {
+        enemyHit = false;
+    }
+
+    void Update()
+    {
+        ReticleRaycast();
+    }
+
+    #region Raycast
+    void ReticleRaycast()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayRange, rayMask))
+        {
+            if (hit.collider.tag == "Enemy" && !enemyHit)
+            {
+                enemyHit = true;
+                Debug.Log("hit(≧▽≦)");
+            }
+        }
+
+        if(hit.collider == null && enemyHit)
+        {
+            enemyHit = false;
+            Debug.Log("out('Д')");
+        }
+
+        #region DebugMode
+        if (debugMode)
+        {
+            Debug.DrawRay(transform.position, transform.forward * rayRange, Color.red);
+        }
+        #endregion
+    }
+    #endregion
+}
