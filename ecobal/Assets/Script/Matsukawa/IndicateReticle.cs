ef7fc2da738f91b2f2a2c39af2fddfe9f1765898
松川修平 <m01144078b@edu.teu.ac.jp>
2016-10-20 19:26:10 +0900

    [松川][追加、変更]ReticleがBalloonに合うとSpriteが変更されるようになりました。PrehabにあるReticleをCanvasの子に入れてもらえれば大丈夫です。またPlayerにRayJudgeを追加すれば機能が実装できます。

---
+++ ecobal/Assets/Script/Matsukawa/IndicateReticle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IndicateReticle : MonoBehaviour
+{
+    public Sprite normalSprite;
+    public Sprite aimSprite;
+    private Sprite indicateSprite;
+
+    private Image reticleImage;
+
+    void Start()
+    {
+        reticleImage = GetComponent<Image>();
+        indicateSprite = normalSprite;
+        reticleImage.sprite = indicateSprite;
+    }
+
+    void IndicateNormal()
+    {
+        reticleImage.sprite = normalSprite;
+    }
+
+    void IndicateAim()
+    {
+        reticleImage.sprite = aimSprite;
+    }
+}
