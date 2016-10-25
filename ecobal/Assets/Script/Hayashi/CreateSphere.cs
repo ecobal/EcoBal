9100f0c60953da540f2690a454f5e4b876279de1
林竜之介 <m011434688@edu.teu.ac.jp>
2016-10-25 16:36:03 +0900

    [林][追加]　頂点に一個づつオブジェクトを置く機能を追加しました

--- ecobal/Assets/Script/Hayashi/CreateSphere.cs
+++ ecobal/Assets/Script/Hayashi/CreateSphere.cs
@@ -1,27 +1,48 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CreateSphere : MonoBehaviour {
 
     MeshFilter mesh;
-    Vector3[] pos = new Vector3[0];
     public GameObject sphere;
 
 	// Use this for initialization
 	void Start () {
         mesh = GetComponent<MeshFilter>();
-        pos = mesh.mesh.vertices;
-        foreach(Vector3 v in pos)
+        List<Vector3> pos = RemoveSamePos(mesh.mesh.vertices);
+        foreach (Vector3 v in pos)
         {
             GameObject obj = Instantiate(sphere, v, Quaternion.Euler(0, 0, 0)) as GameObject;
             obj.transform.parent = transform;
 
         }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    List<Vector3> RemoveSamePos(Vector3[] PosArray)
+    {
+        int count = 0;
+        List<Vector3> PosList = new List<Vector3>(PosArray);
+        for (int j = 0; j<PosList.Count;j++)
+        {
+            for (int i = 0; i < PosList.Count; i++)
+            {
+                if (i == j) continue;
+                else
+                {
+                    if (Vector3.Distance(PosList[j], PosList[i]) <= 0.0001f) PosList.RemoveAt(i);
+                }
+            }
+        }
+        return PosList;
+
+    }
 }
