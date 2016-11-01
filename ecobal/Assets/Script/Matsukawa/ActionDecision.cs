753db99877996d09792d273be4219d79c4e5c698
松川修平 <m01144078b@edu.teu.ac.jp>
2016-11-02 03:16:38 +0900

    [松川][追加]BossのAIの実装。Boss戦時にアイテムがプレイヤーの周囲によって来る仕様の実装。

---
+++ ecobal/Assets/Script/Matsukawa/ActionDecision.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionDecision : MonoBehaviour
+{
+    #region Interval
+    public float maxActionInterval;
+    public float minActionInterval;
+    private float actionInterval;
+    private float intervalTime;
+    private bool isInterval;
+    #endregion
+
+    public string[] actionName;
+    private int actionNumber;
+
+    void Start()
+    {
+        isInterval = true;
+        actionInterval = maxActionInterval / 2;
+    }
+
+    void Update()
+    {
+        if (!isInterval) ActDecision();
+
+        actionNumber = Random.Range(0, 5);
+        IntervalProcessing();
+    }
+
+    void DecisionInterval()
+    {
+        actionInterval = Random.Range(minActionInterval, maxActionInterval);
+        isInterval = true;
+        // IntervalTime Debug
+        //Debug.Log(actionInterval);
+    }
+
+    void IntervalProcessing()
+    {
+        if (isInterval) intervalTime += 1 * Time.deltaTime;
+
+        if (intervalTime >= actionInterval)
+        {
+            isInterval = false;
+            intervalTime = 0;
+        }
+    }
+
+    void ActDecision()
+    {
+        actionNumber = Random.Range(0, actionName.Length) % actionName.Length;
+        // ActionNumber Debug
+        //Debug.Log(actionNumber);
+        
+        SendMessage(actionName[actionNumber]);
+        DecisionInterval();
+    }
+}
---
+++ ecobal/Assets/Script/Matsukawa/DropItemMotion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropItemMotion : MonoBehaviour
+{
+    public float rangeValue;
+    private float objectiveRange;
+
+    private GameObject playerObject;
+    private Vector3 objectivePoint;
+
+    void Start()
+    {
+        ObjectivePointDecide();
+    }
+
+    void Update()
+    {
+        ItemMotion();
+    }
+
+    void ItemMotion()
+    {
+        transform.position = Vector3.Lerp(transform.position, objectivePoint, 1 * Time.deltaTime);
+    }
+
+    void ObjectivePointDecide()
+    {
+        objectiveRange = Random.Range(0, rangeValue);
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        objectivePoint = playerObject.transform.position + playerObject.transform.TransformVector(objectiveRange, objectiveRange, 0) + playerObject.transform.forward * -1;
+    }
+}
