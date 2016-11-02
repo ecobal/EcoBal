ea4fb046c251bc0ef0556d18b69ff8ef6e36032e
松川修平 <m01144078b@edu.teu.ac.jp>
2016-11-02 18:36:56 +0900

    [松川][追加、変更]追加はTitleで新しい演出を思いついたので追加しました。要改善。変更点はBossのAIの改善などを行いました。

---
+++ ecobal/Assets/Script/Matsukawa/BalloonAppearance.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonAppearance : MonoBehaviour
+{
+    public GameObject[] balloon;
+    private int randValue;
+    private float appearPoint;
+    private Vector3 objectPosition;
+
+    private bool appear;
+    public float appearTime;
+    private float interval;
+
+    void Start()
+    {
+        interval = 0;
+        objectPosition = transform.position;
+        AppearBalloon();
+    }
+
+    void Update()
+    {
+        if (appear) AppearBalloon();
+        else if (!appear) AppearInterval();
+    }
+
+    void AppearBalloon()
+    {
+        randValue = Random.Range(0, 100) % balloon.Length;
+        appearPoint = Random.Range(-transform.localScale.x / 2, transform.localScale.x / 2);
+        Instantiate(balloon[randValue], objectPosition + new Vector3(appearPoint, 0, 0), transform.rotation);
+        appear = false;
+        interval = 0;
+    }
+
+    void AppearInterval()
+    {
+        interval += 1 * Time.deltaTime;
+        if (interval >= appearTime) appear = true;
+    }
+}
---
+++ ecobal/Assets/Script/Matsukawa/TitleBalloonMotion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleBalloonMotion : MonoBehaviour
+{
+    private float sinRadius;
+    private float cosRadius;
+    private Vector3 moveVector;
+
+    public float floatRange = 1.75f;
+    public float sideRange = 0.01f;
+    public float sideValue = 2.0f;
+    
+    Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        sinRadius = Random.Range(0, 359);
+        cosRadius = Random.Range(0, 359);
+    }
+
+    void Update()
+    {
+        moveVector.y = floatRange * Mathf.Sin(sinRadius + Time.realtimeSinceStartup);
+        moveVector.x = sideRange + Mathf.Cos(cosRadius + (Time.realtimeSinceStartup / sideValue));
+        if (Mathf.Sin(sinRadius + Time.realtimeSinceStartup) >= 0) moveVector.y = moveVector.y * 1.2f;
+        rigidbody.velocity = transform.TransformVector(moveVector);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Respawn") Destroy(gameObject);
+    }
+}
