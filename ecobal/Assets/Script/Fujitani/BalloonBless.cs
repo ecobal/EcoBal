845556fdcd3deaafcd4c12765fae33a16ab917ef
G016A1547\naokifujitani <n009fuifuji@gmail.com>
2016-11-02 20:59:08 +0900

    [追加] Script ・SummonsBalloon ・BalloonBless  を追加。どちらもボスの攻撃

---
+++ ecobal/Assets/Script/Fujitani/BalloonBless.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BalloonBless : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject balloon;
+
+    [SerializeField]
+    private int balloonCount;
+    [SerializeField]
+    private int blessCount;
+    [SerializeField]
+    private float range;
+    [SerializeField]
+    private float blessSpeed;
+    [SerializeField]
+    private int blessFrequency;
+    [SerializeField]
+    private int blessStartPositionZ;
+
+    private List<GameObject> bomBalloonList;
+
+    private int count;
+    private int rigitCount;
+
+    private bool blessStart;
+
+    // Use this for initialization
+    void Start()
+    {
+        count = 0;
+        bomBalloonList = new List<GameObject>();
+        blessStart = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count++;
+
+        if (count > blessCount)
+        {
+            blessStart = false;
+        }
+        if (blessStart && count % blessFrequency == 0)
+        {
+            rigitCount++;
+            BalloonBlessUpdate();
+        }
+
+        for (int i = 0; i < balloonCount * rigitCount; i++)
+        {
+            bomBalloonList[i].transform.rotation = Quaternion.Euler(
+                Random.Range(-range, range),
+                Random.Range(-range, range),
+                 Random.Range(-range, range)
+                 );
+
+            bomBalloonList[i].GetComponent<Rigidbody>().AddForce(
+                bomBalloonList[i].transform.forward * blessSpeed
+               );
+        }
+    }
+
+
+    void BalloonBlessUpdate()
+    {
+        for (int i = 0; i < balloonCount; i++)
+        {
+            GameObject obj = Instantiate(balloon, transform.position + transform.forward * blessStartPositionZ, transform.rotation) as GameObject;
+            bomBalloonList.Add(obj);
+            obj.gameObject.transform.parent = this.gameObject.transform;
+
+        }
+
+    }
+
+    public void OnBlessMode()
+    {
+        blessStart = true;
+    }
+
+    public void OffBressMode()
+    {
+        blessStart = false;
+    }
+}
---
+++ ecobal/Assets/Script/Fujitani/SummonsBalloon.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SummonsBalloon : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject balloon;
+
+    [SerializeField]
+    private int balloonCount;
+    [SerializeField]
+    private float rangeMin;
+    [SerializeField]
+    private float rangeMax;
+
+    private bool summonsStart;
+
+    private List<GameObject> balloonList;
+
+    // Use this for initialization
+    void Start()
+    {
+        balloonList = new List<GameObject>();
+        summonsStart = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (summonsStart) SummonsBalloonUpdate();
+    }
+
+    void SummonsBalloonUpdate()
+    {
+        // 生成
+        for (int i = 0; i < balloonCount; ++i)
+        {
+            GameObject obj = Instantiate(balloon, transform.position, transform.rotation) as GameObject;
+            // リストに追加
+            balloonList.Add(obj);
+            // 親子関係
+            obj.gameObject.transform.parent = this.gameObject.transform;
+        }
+
+        // ランダムで角度を指定
+        for (int i = 0; i < balloonList.Count; ++i)
+        {
+            balloonList[i].gameObject.transform.eulerAngles =
+                new Vector3(
+                    Random.Range(0.0f, 360.0f),
+                    Random.Range(0.0f, 360.0f),
+                    Random.Range(0.0f, 360.0f));
+        }
+
+        // 向いている方向に移動
+        for (int i = 0; i < balloonList.Count; ++i)
+        {
+            balloonList[i].transform.position += balloonList[i].transform.forward * Random.Range(rangeMin, rangeMax);
+        }
+        summonsStart = false;
+
+    }
+
+    public void OnSummonsMode()
+    {
+        summonsStart = true;
+    }
+
+    public void OffSummonsMode()
+    {
+        summonsStart = false;
+    }
+}
